79d4c1ac6ac9f137cf25dc3d76f142022a11e6f2
NajamShehzad <najambutt195@gmail.com>
2018-12-01 21:26:08 +0500

    Map

--- Client/App.js
+++ Client/App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import Phone from './src/firebase/firebasePhone';
 import firebase from 'firebase';
 import Navigation from './navigation/Navigation';
-import { Permissions, Location } from 'expo';
+import MapView from './components/map/map';
 
 
 
@@ -11,10 +11,8 @@ import { Permissions, Location } from 'expo';
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text>
-          SOme text here
-        </Text>
+      <View style={{ flex: 1 }}>
+        <MapView/>
       </View>
     );
   }
--- Client/components/map/map.js
+++ Client/components/map/map.js
@@ -1,1 +1,78 @@
-import MapView from 'react-native-map';
+import { MapView as Map12, Notifications, Permissions, Location } from 'expo';
+import React, { Component } from "react";
+import MapView, { Circle } from 'react-native-maps'
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+export default class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            locationResult: null,
+            location: { coords: { latitude: 24.926294, longitude: 67.022095 } },
+            marker: false
+        };
+    }
+
+
+    componentDidMount() {
+        this._getLocationAsync();
+    }
+
+
+
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                locationResult: 'Permission to access location was denied',
+                location,
+            });
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        this.setState({ locationResult: JSON.stringify(location), location, marker: true });
+    };
+
+    setMarkers() {
+        return (
+            < Circle
+                draggable
+                center={this.state.location.coords}
+                title={"Current Location"}
+                radius={100}
+                strokeColor="#ffffff"
+                fillColor="#e6f9ff"
+                strokeWidth={2}
+            >
+            </ Circle>
+        )
+    }
+
+    render() {
+        const { location, marker } = this.state;
+        console.log(Map12);
+        return (
+            <MapView
+                style={{ flex: 1 }}
+                region={{ latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }}
+            >
+                {
+                    marker ?
+                        this.setMarkers() :
+                        < Circle
+                            center={this.state.location.coords}
+                            title={"Current Location"}
+                            radius={100}
+                            strokeColor="#4dd2ff"
+                            fillColor="#e6f9ff"
+                            strokeWidth={1}
+                            zIndex={0}
+                        >
+                        </ Circle>
+                }
+            </MapView>
+        )
+    }
+}
